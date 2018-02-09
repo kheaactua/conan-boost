96223918456fcd957d4062ee7003741550114f29
Matthew Russell <mrussell@neptec.com>
2018-02-09 14:34:52 -0500

    Removed deprecated (now missing) conan_directory attribute

--- test/conanfile.py
+++ test/conanfile.py
@@ -17,16 +17,17 @@ class DefaultNameConan(ConanFile):
     settings = "os", "compiler", "arch", "build_type"
     requires = "boost/1.60.0@%s/%s" % (username, channel)
     generators = "cmake"
+    exports  = "data.txt"
 
     def config(self):
         if self.options["boost"].header_only:
             self.settings.clear()
 
     def build(self):
-        cmake = CMake(self.settings)
+        cmake = CMake(self)
         header_only = "-DHEADER_ONLY=TRUE " if self.options["boost"].header_only else ""
         with_python = "-DWITH_PYTHON=True" if self.options["boost"].python else ""
-        self.run('cmake "%s" %s %s %s' % (self.conanfile_directory, cmake.command_line, header_only, with_python))
+        self.run('cmake "%s" %s %s %s' % (self.source_folder, cmake.command_line, header_only, with_python))
         self.run("cmake --build . %s" % cmake.build_config)
 
     def imports(self):
@@ -34,7 +35,7 @@ class DefaultNameConan(ConanFile):
         self.copy(pattern="*.dylib", dst="bin", src="lib")
         
     def test(self):        
-        data_file = os.path.join(self.conanfile_directory, "data.txt")
+        data_file = os.path.join(self.source_folder, "data.txt")
         self.run("cd bin && .%slambda < %s" % (os.sep, data_file))
         if not self.options["boost"].header_only:
             self.run("cd bin && .%sregex_exe < %s" % (os.sep, data_file))
